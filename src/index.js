a5413551f6e990aab7f04dbb7ad56e5c3c79d3ce
dicky azhari hernuwan <hernuwandicky@gmail.com>
2022-07-29 20:20:23 +0700

    callback

---
+++ src/index.js
@@ -0,0 +1,17 @@
+function getData(url, cb) {
+    let xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        return cb(JSON.parse(xhr.responseText));
+      }
+    };
+    xhr.open("GET", url);
+    xhr.send();
+  }
+  
+  const data = getData("https://jsonplaceholder.typicode.com/users", function(data) {
+      console.log(data);
+      var responseText = document.getElementById('app');
+      responseText.innerHTML =JSON.stringify(data);
+  });
+  
